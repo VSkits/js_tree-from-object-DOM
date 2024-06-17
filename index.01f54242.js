28493ccf6cbfa792feb496b3e33c8d8b74c147a9
Victor Skits <vityok.nice@gmail.com>
2024-06-17 12:33:22 +0300

    make build

---
+++ index.01f54242.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("li"),l=document.createElement("ul");a.textContent=r,e(l,t[r]),l.hasChildNodes()&&a.append(l),n.append(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.01f54242.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    for(const itemType in data){
+        const li = document.createElement("li");
+        const ul = document.createElement("ul");
+        li.textContent = itemType;
+        createTree(ul, data[itemType]);
+        if (ul.hasChildNodes()) li.append(ul);
+        element.append(li);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
